fb42d5b22eb3a6176065aa5c4e6f110e593eb28c
happy-roman <h0m0s@mail.ru>
2026-04-22 16:37:19 +0800

    fix names in store

--- server/controllers/cartController.js
+++ server/controllers/cartController.js
@@ -3,16 +3,4 @@ const { cart } = require('../models');
 
 module.exports = {
   ...crudController(cart),
-  async getAll(req, res) {
-    try {
-      console.log(req.body.userId);
-      // `${req.body.userId}`
-      const userCart = await cart.find({ userId: '637da7066cdf70aa8d620970' });
-      // eslint-disable-next-line no-underscore-dangle
-      const items = await cart.findById(userCart[0]._id);
-      return res.status(200).send(items);
-    } catch (err) {
-      throw new Error(err);
-    }
-  },
 };
